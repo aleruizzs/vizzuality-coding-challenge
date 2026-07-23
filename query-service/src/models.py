8077bb6d9671f21528d5399fda084fe914757b60
Alejandro Ruiz <121815628+aleruizzs@users.noreply.github.com>
2026-07-23 19:51:46 +0200

    PR-6 - SQLAlchemy (#7)
    updated model to use sqlalchemy code

--- query-service/src/models.py
+++ query-service/src/models.py
@@ -1,13 +1,20 @@
-from sqlalchemy import Column, Float, Integer, String
-from src.database import Base
+from typing import Optional
+from sqlalchemy import Double, Integer, String
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
 
 
 class Emission(Base):
     __tablename__ = "emissions"
 
-    id = Column(Integer, primary_key=True)
-    country = Column(String, index=True, nullable=False)
-    sector = Column(String, index=True, nullable=False)
-    parent_sector = Column(String, index=True, nullable=True)
-    year = Column(Integer, nullable=False)
-    value = Column(Float, nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    country: Mapped[str] = mapped_column(String(255), index=True, nullable=False)
+    sector: Mapped[str] = mapped_column(String(255), index=True, nullable=False)
+    parent_sector: Mapped[Optional[str]] = mapped_column(
+        String(255), index=True, nullable=True
+    )
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    value: Mapped[Optional[float]] = mapped_column(Double, nullable=True)

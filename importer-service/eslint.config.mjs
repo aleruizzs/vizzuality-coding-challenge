299c337c46ec0a9955fc332e65cb1975911e660b
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-07-23 13:31:03 +0200

    added lint and updated ci

---
+++ importer-service/eslint.config.mjs
@@ -0,0 +1,23 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+
+export default [
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    ignores: ['dist/**', 'node_modules/**'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+        ecmaVersion: 'latest',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];

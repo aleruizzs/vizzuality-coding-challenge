f6c169fe4da9d4d0654ec9e4677c0e4f11185e50
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-07-23 12:25:34 +0200

    basic implementation of http

--- importer-service/src/index.ts
+++ importer-service/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Importer service starting...");
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/health', (_req: Request, res: Response) => {
+  res.status(200).json({ status: 'ok', service: 'importer-service' });
+});
+
+app.listen(3000, () => {
+  console.log('Importer service is running on port 3000');
+});

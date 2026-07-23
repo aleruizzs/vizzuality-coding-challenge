dc028f53ba38a8b8ef613e32f9fdad46db63d9c5
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-07-23 18:08:58 +0200

    failing ci

--- query-service/src/database.py
+++ query-service/src/database.py
@@ -9,7 +9,7 @@ engine = create_engine(
     max_overflow=10,
     pool_timeout=30,
     pool_recycle=1800,
-    pool_pre_ping=True
+    pool_pre_ping=True,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 

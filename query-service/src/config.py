f7b8d3ec689ea887b2076c570099fe87d1c002f9
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-07-26 19:56:43 +0200

    ensured route

--- query-service/src/config.py
+++ query-service/src/config.py
@@ -1,10 +1,15 @@
+from pathlib import Path
+
 from pydantic import computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_FILE_PATH = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=ENV_FILE_PATH,
         env_file_encoding="utf-8",
         extra="ignore",
     )
